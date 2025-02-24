f77c5c15929206850f995a7fa5e76d887a6790dc
Mustafa Zorlu <mzorlu3579@gmail.com>
2025-02-24 22:53:45 +0300

    typescript

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+const person = {
+    name: "John"
+};
+let myName = "Mustafa";
+myName = 'Habibe';
+let favNumber = 2;
+let tsHard = false;
+console.log(myName);
+console.log(tsHard);
